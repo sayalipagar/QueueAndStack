e57d6668833da17557141ce09c159aed2b998bec
sayalipagar <pagarsayali565@gmail.com>
2022-09-17 20:54:55 +0530

    [sayali] Add:PeakInStack

--- StackAndQueueProgramm/Program.cs
+++ StackAndQueueProgramm/Program.cs
@@ -7,17 +7,24 @@
                 bool flag = true;
                 while (flag)
                 {
-                Console.WriteLine("\n Select Your Choice: 1.PushElement ");
+                Console.WriteLine("\n\n Select Your Choice \n 1.PushElement\n 2.PeakElement ");
                 int option = Convert.ToInt32(Console.ReadLine());
                     switch (option)
                     {
-                        case 1:
-                            Console.WriteLine("----------------------------------\n");
-                            Stack stack = new Stack();
-                            stack.Push(56);
-                            stack.Push(30);
-                            stack.Push(70);
+                    case 1:
+                        Stack stack = new Stack();
+                        stack.Push(56);
+                        stack.Push(30);
+                        stack.Push(70);
                         break;
+                    case 2:
+                        Stack stack1 = new Stack();
+                        stack1.Push(56);
+                        stack1.Push(30);
+                        stack1.Push(40);
+                        stack1.Push(70);
+                        stack1.Peek();
+                       break;
                     default:
                         Console.WriteLine("Invalid Choice");
                         break;
--- StackAndQueueProgramm/Stack.cs
+++ StackAndQueueProgramm/Stack.cs
@@ -27,6 +27,15 @@ namespace StackAndQueueProgramm
             this.top = node;
             Console.WriteLine("{0} pushed to stack", value);
         }
+        internal void Peek()                                
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is Empty");
+                return;
+            }
+            Console.WriteLine("\n{0} is in the top of the Stack", this.top.data);
+        }
         internal void Dispaly()
         {
             Node temp = this.top;
