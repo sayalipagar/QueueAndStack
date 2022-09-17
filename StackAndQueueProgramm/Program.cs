79e613b9a103eb614fdf44292174e8834e4d9422
sayalipagar <pagarsayali565@gmail.com>
2022-09-17 20:39:05 +0530

    [sayali] Add:PushInStack

--- StackAndQueueProgramm/Program.cs
+++ StackAndQueueProgramm/Program.cs
@@ -3,8 +3,27 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome Stack and Queue Problem Statement!");
+            {
+                bool flag = true;
+                while (flag)
+                {
+                Console.WriteLine("\n Select Your Choice: 1.PushElement ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                    switch (option)
+                    {
+                        case 1:
+                            Console.WriteLine("----------------------------------\n");
+                            Stack stack = new Stack();
+                            stack.Push(56);
+                            stack.Push(30);
+                            stack.Push(70);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Choice");
+                        break;
+
+                    }
+                }
+            }
         }
     }
-}
